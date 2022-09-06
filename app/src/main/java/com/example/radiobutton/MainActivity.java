75547e9744a42ce8ed9f74cb5c4dbbebee9249eb
Rakendra <aznil.rakendra@gmail.com>
2022-09-06 21:51:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.radiobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final RadioGroup rgJawaban1 = (RadioGroup) findViewById(R.id.rgJawaban1);
+        Button btSubmit = (Button) findViewById(R.id.btnJawab);
+        btSubmit.setOnClickListener((view) -> {
+            int id = rgJawaban1.getCheckedRadioButtonId();
+            switch (id){
+                case R.id.jwb1:
+                    Toast.makeText(getApplicationContext(), "Anda Salah Jika Memilih"+
+                            ((RadioButton) findViewById(id)).getText(), Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.jwb2:
+                    Toast.makeText(getApplicationContext(), "Anda Salah memilih"+
+                            ((RadioButton)findViewById(id)).getText(), Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.jwb3:
+                    Toast.makeText(getApplicationContext(), "Anda Benar jika memilih" +
+                            ((RadioButton)findViewById(id)).getText(), Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        });
+    }
+}
